4b3725c053946d41e3d967acd02de50dd146573c
pedrinhomotivado1 <pedroginofera@gmail.com>
2023-04-16 14:28:04 -0300

    novas alterações

--- js.js
+++ js.js
@@ -46,27 +46,21 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
-
-/*----------------------------- area modal ------------------------------ */
-// Get the modal
+////////////////////////////////////// inicio do modal////////////////////////////////////////////////////
+////////// Get the modal
 var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
+/////// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
+//////// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
+///////// When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
