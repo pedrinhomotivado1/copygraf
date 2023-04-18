4a8391c2c4391172333c2e2a0c76ecc8b74a3535
pedrinhomotivado1 <pedroginofera@gmail.com>
2023-04-18 20:24:20 -0300

    novo

--- js.js
+++ js.js
@@ -52,13 +52,13 @@ var modal0 = document.getElementById("myModal0");
 // Get the button that opens the modal
 var btn0 = document.getElementById("myBtn0");
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closemodal")[0];
+var span0 = document.getElementsByClassName("closemodal")[0];
 // When the user clicks on the button, open the modal
 btn0.onclick = function() {
   modal0.style.display = "block";
 }
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span0.onclick = function() {
   modal0.style.display = "none";
 }
 // When the user clicks anywhere outside of the modal, close it
